c5bcc1f7ee84267c45a5f76419f14c9279263547
bsusta <susta@lansystems.sk>
2017-05-11 14:11:35 +0200

    S16L112 start

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCOomcJAJGJJXnPlskon3xmjzJJfG1kcOY',
+      authDomain: 'manager-546dd.firebaseapp.com',
+      databaseURL: 'https://manager-546dd.firebaseio.com',
+      projectId: 'manager-546dd',
+      storageBucket: 'manager-546dd.appspot.com',
+      messagingSenderId: '1057199920568'
+    };
+    firebase.initializeApp(config);
+  }
+
+
   render() {
     return (
-      <Provider>
-        <View>
-          <Text>
-            Hello!
-          </Text>
-        </View>
+      <Provider store={createStore(reducers)}>
+          <LoginForm />
       </Provider>
     );
   }
