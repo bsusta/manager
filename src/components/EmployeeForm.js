38b7c098f71fe486f6abc9d5a3fa4590cdb60fc5
Brano Susta <susta@lansystems.sk>
2017-05-29 16:23:44 +0200

    S19L156 start

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { View, Text, Picker } from 'react-native';
 import { employeeUpdate } from '../actions';
 import { CardSection, Input } from './common';
+
 
 class EmployeeForm extends Component {
   render() {
@@ -52,4 +54,9 @@ const styles = {
   }
 };
 
-export default EmployeeForm;
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+   return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeForm);
